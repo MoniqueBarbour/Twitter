09869c7bd40817a525f88fc4322cfec5dd3c8c95
Monique Barbour <moniquebarbour15@gmail.com>
2020-02-17 23:44:50 -0500

    Updated Changes

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Monique Barbour on 2/17/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var userNameLabel: UILabel!
+    @IBOutlet var tweetContent: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
